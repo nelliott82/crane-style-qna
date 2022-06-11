ba393085c2a5febafd7348fd141d33be1281ed31
nelliott82 <nikkoelliott@gmail.com>
2022-06-11 10:17:14 -0400

    Added puts for helpful and reported and a replace for single quotes on insert

--- server/controllers/answers.js
+++ server/controllers/answers.js
@@ -19,8 +19,27 @@ module.exports = {
         res.statusCode = 400;
         res.end(JSON.stringify(err));
       } else {
-        res.statusCode = 200;
-        res.end(JSON.stringify(results));
+        res.sendStatus(201);
+      }
+    });
+  },
+  putHelpful: function (req, res) {
+    models.answers.putHelpful(req.params.answer_id, function(err, results) {
+      if (err) {
+        res.statusCode = 400;
+        res.end(JSON.stringify(err));
+      } else {
+        res.sendStatus(204);
+      }
+    });
+  },
+  putReported: function (req, res) {
+    models.answers.putReported(req.params.answer_id, function(err, results) {
+      if (err) {
+        res.statusCode = 400;
+        res.end(JSON.stringify(err));
+      } else {
+        res.sendStatus(204);
       }
     });
   },
