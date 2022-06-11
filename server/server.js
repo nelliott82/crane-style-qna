2f4b7ba44a1afe307c75e9be7c85a2cd0ff0a142
nelliott82 <nikkoelliott@gmail.com>
2022-06-11 11:10:13 -0400

    Added morgan and cors to server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+var routerQuestions = require('./routesQuestions.js');
+var routerAnswers = require('./routesAnswers.js');
+var morgan = require('morgan');
+var cors = require('cors');
 const app = express();
 const port = 8080;
 
-var routerQuestions = require('./routesQuestions.js');
-var routerAnswers = require('./routesAnswers.js');
-
+app.use(morgan('dev'));
+app.use(cors());
 app.use(express.json());
 
 app.use('/questions', routerQuestions);
