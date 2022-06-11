bebd43c5e1a700de51db83ca37c342e96b93cef4
nelliott82 <nikkoelliott@gmail.com>
2022-06-10 20:50:26 -0400

    Added puts working, but post stopped working for some reason.

--- server/controllers/questions.js
+++ server/controllers/questions.js
@@ -13,13 +13,34 @@ module.exports = {
     });
   },
   post: function (req, res) {
-    console.log(req.body);
     models.questions.post(req.body, function(err, results) {
       if (err) {
         res.statusCode = 400;
         res.end(JSON.stringify(err));
       } else {
-        res.statusCode = 200;
+        res.statusCode = 201;
+        res.end(JSON.stringify(results));
+      }
+    });
+  },
+  putHelpful: function (req, res) {
+    models.questions.putHelpful(req.params.question_id, function(err, results) {
+      if (err) {
+        res.statusCode = 400;
+        res.end(JSON.stringify(err));
+      } else {
+        res.statusCode = 204;
+        res.end(JSON.stringify(results));
+      }
+    });
+  },
+  putReported: function (req, res) {
+    models.questions.putReported(req.params.question_id, function(err, results) {
+      if (err) {
+        res.statusCode = 400;
+        res.end(JSON.stringify(err));
+      } else {
+        res.statusCode = 204;
         res.end(JSON.stringify(results));
       }
     });
