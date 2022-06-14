13e0ccc323cf2834f09a00ceb575370b6f8bf760
nelliott82 <nikkoelliott@gmail.com>
2022-06-14 17:34:21 -0400

    Removed morgan and cors

--- server/server.js
+++ server/server.js
@@ -1,13 +1,9 @@
 const express = require('express');
 var routerQuestions = require('./routesQuestions.js');
 var routerAnswers = require('./routesAnswers.js');
-var morgan = require('morgan');
-var cors = require('cors');
 const app = express();
 const port = 8080;
 
-app.use(morgan('dev'));
-app.use(cors());
 app.use(express.json());
 
 app.use('/questions', routerQuestions);
