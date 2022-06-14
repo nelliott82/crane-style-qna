d31a9989b53113614535d6c2c6e3d977f1bd5c5e
nelliott82 <nikkoelliott@gmail.com>
2022-06-14 17:32:14 -0400

    k6 getAnswers tests

--- k6_tests/getAnswersScript.js
+++ k6_tests/getAnswersScript.js
@@ -3,21 +3,27 @@ import { check, sleep } from 'k6';
 
 // export const options = {
 //   stages: [
-//     { duration: '30s', target: 20 },
-//     { duration: '1m30s', target: 10 },
-//     { duration: '20s', target: 0 },
+//     { duration: '30s', target: 0 },
+//     { duration: '30s', target: 10 },
+//     { duration: '1m', target: 100 },
+//     { duration: '1m', target: 1000 },
+//     { duration: '1m', target: 10000 },
+//     { duration: '1m', target: 1000 },
+//     { duration: '1m', target: 100 },
+//     { duration: '30s', target: 10 },
+//     { duration: '30s', target: 0 },
 //   ],
 // };
 export const options = {
   stages: [
-    { duration: '30s', target: 20 }
+    { duration: '10s', target: 10 }
   ],
 };
 
 export default function () {
   // var res = http.get('https://test.k6.io');
-  var question_id = Math.floor(Math.random() * 3000000);
+  var question_id = Math.floor(Math.random() * (3518963 - 3167067)) + 3167067;
   var res = http.get(`http://localhost:8080/answers/${question_id}`);
   check(res, { 'status was 200': (r) => r.status == 200 });
-  sleep(1);
+  sleep(0.001);
 }
