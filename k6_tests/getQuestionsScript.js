43cc8be29fd68b656e694a149037ccda0fc89b7d
nelliott82 <nikkoelliott@gmail.com>
2022-06-14 17:32:44 -0400

    k6 getQuestions tests

--- k6_tests/getQuestionsScript.js
+++ k6_tests/getQuestionsScript.js
@@ -1,18 +1,28 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
+// export const options = {
+//   stages: [
+//     { duration: '30s', target: 0 },
+//     { duration: '30s', target: 10 },
+//     { duration: '1m', target: 100 },
+//     { duration: '1m', target: 1000 },
+//     { duration: '1m', target: 10000 },
+//     { duration: '1m', target: 1000 },
+//     { duration: '1m', target: 100 },
+//     { duration: '30s', target: 10 },
+//     { duration: '30s', target: 0 },
+//   ],
+// };
 export const options = {
   stages: [
-    { duration: '30s', target: 20 },
-    { duration: '1m30s', target: 10 },
-    { duration: '20s', target: 0 },
+    { duration: '10s', target: 10 }
   ],
 };
 
 export default function () {
-  // var res = http.get('https://test.k6.io');
-  var product_id = Math.floor(Math.random() * 1000000);
+  var product_id = Math.floor(Math.random() * (1000011 - 900000)) + 900000;
   var res = http.get(`http://localhost:8080/questions/${product_id}`);
   check(res, { 'status was 200': (r) => r.status == 200 });
-  sleep(1);
+  sleep(0);
 }
